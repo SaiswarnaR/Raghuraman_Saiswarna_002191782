6eec909a8fb997a118514168d7fa3d80bbb0ac08
SaiswarnaR <saiswarna.r@northeastern.edu>
2021-09-26 19:49:33 -0400

    Assignment-1

---
+++ Assignment1/src/model/Person.java
@@ -0,0 +1,239 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import java.awt.Image;
+import java.math.BigInteger;
+import java.util.Date;
+
+/**
+ *
+ * @author Sai
+ */
+public class Person {
+
+    private String name;
+    private String addressLine1;
+    private String addressLine2;
+    private String city;
+    private String state;
+    private String zipCode;
+    private Date dateofBirth;
+    private String telephoneNumber;
+    private BigInteger faxNumber;
+    private String emailAddress;
+    private String socialsecurityNumber;
+    private BigInteger medicalrecordNumber;
+    private BigInteger healthplanNumber;
+    private String bankaccountNumber;
+    private String certificatelicenseNumber;
+    private String vehicleIdentifier;
+    private String deviceIdentifier;
+    private String linkedIn;
+    private String ipAddress;
+    private Image biometricIdentifier;
+    private Image profileImage;
+    private String uniqueIdentifier;
+    private String profileImagepath;
+    private String biometricImagepath;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddressLine1() {
+        return addressLine1;
+    }
+
+    public void setAddressLine1(String addressLine1) {
+        this.addressLine1 = addressLine1;
+    }
+
+    public String getAddressLine2() {
+        return addressLine2;
+    }
+
+    public void setAddressLine2(String addressLine2) {
+        this.addressLine2 = addressLine2;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(String zipCode) {
+        this.zipCode = zipCode;
+    }
+
+    public Date getDateofBirth() {
+        return dateofBirth;
+    }
+
+    public void setDateofBirth(Date dateofBirth) {
+        this.dateofBirth = dateofBirth;
+    }
+
+    public String getTelephoneNumber() {
+        return telephoneNumber;
+    }
+
+    public void setTelephoneNumber(String telephoneNumber) {
+        this.telephoneNumber = telephoneNumber;
+    }
+
+    public BigInteger getFaxNumber() {
+        return faxNumber;
+    }
+
+    public void setFaxNumber(BigInteger faxNumber) {
+        this.faxNumber = faxNumber;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    public String getSocialsecurityNumber() {
+        return socialsecurityNumber;
+    }
+
+    public void setSocialsecurityNumber(String socialsecurityNumber) {
+        this.socialsecurityNumber = socialsecurityNumber;
+    }
+
+    public BigInteger getMedicalrecordNumber() {
+        return medicalrecordNumber;
+    }
+
+    public void setMedicalrecordNumber(BigInteger medicalrecordNumber) {
+        this.medicalrecordNumber = medicalrecordNumber;
+    }
+
+    public BigInteger getHealthplanNumber() {
+        return healthplanNumber;
+    }
+
+    public void setHealthplanNumber(BigInteger healthplanNumber) {
+        this.healthplanNumber = healthplanNumber;
+    }
+
+    public String getBankaccountNumber() {
+        return bankaccountNumber;
+    }
+
+    public void setBankaccountNumber(String bankaccountNumber) {
+        this.bankaccountNumber = bankaccountNumber;
+    }
+
+    public String getCertificatelicenseNumber() {
+        return certificatelicenseNumber;
+    }
+
+    public void setCertificatelicenseNumber(String certificatelicenseNumber) {
+        this.certificatelicenseNumber = certificatelicenseNumber;
+    }
+
+    public String getVehicleIdentifier() {
+        return vehicleIdentifier;
+    }
+
+    public void setVehicleIdentifier(String vehicleIdentifier) {
+        this.vehicleIdentifier = vehicleIdentifier;
+    }
+
+    public String getDeviceIdentifier() {
+        return deviceIdentifier;
+    }
+
+    public void setDeviceIdentifier(String deviceIdentifier) {
+        this.deviceIdentifier = deviceIdentifier;
+    }
+
+    public String getLinkedIn() {
+        return linkedIn;
+    }
+
+    public void setLinkedIn(String linkedIn) {
+        this.linkedIn = linkedIn;
+    }
+
+    public String getIpAddress() {
+        return ipAddress;
+    }
+
+    public void setIpAddress(String ipAddress) {
+        this.ipAddress = ipAddress;
+    }
+
+    public Image getBiometricIdentifier() {
+        return biometricIdentifier;
+    }
+
+    public void setBiometricIdentifier(Image biometricIdentifier) {
+        this.biometricIdentifier = biometricIdentifier;
+    }
+
+    public Image getProfileImage() {
+        return profileImage;
+    }
+
+    public void setProfileImage(Image profileImage) {
+        this.profileImage = profileImage;
+    }
+
+    public String getUniqueIdentifier() {
+        return uniqueIdentifier;
+    }
+
+    public void setUniqueIdentifier(String uniqueIdentifier) {
+        this.uniqueIdentifier = uniqueIdentifier;
+    }
+    
+     public String getprofileImagepath() {
+        return profileImagepath;
+    }
+
+    public void setprofileImagepath(String profileImagepath) {
+        this.profileImagepath = profileImagepath;
+    }
+    
+      public String getbiometricImagepath() {
+        return biometricImagepath;
+    }
+
+    public void setbiometricImagepath(String biometricImagepath) {
+        this.biometricImagepath = biometricImagepath;
+    }
+    
+   
+   
+}
+
+
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Raghuraman_Saiswarna_002191782
+
+Name : Saiswarna Raghuraman
+
+Assignment 1 : Create a person class and View a person class with field validations
